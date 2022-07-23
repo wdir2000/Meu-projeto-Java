09f5e4b320ea5f6a2b65e94e84fee158f54372e6
waldir <wdir2000@yahoo.com.br>
2022-07-23 00:44:18 -0300

    Exibindo e manipulando os dados e criando classe json parser

--- src/App.java
+++ src/App.java
@@ -3,18 +3,17 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.security.Key;
 import java.util.List;
 import java.util.Map;
 
 public class App {
 
-    private static String key;
-
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os filmes
-        String url = "https://api.themoviedb.org/3/movie/550?api_key=7b4c22a6e2407b6b1d1523351e31c484";
+        //String url = "https://api.themoviedb.org/3/trending/movie/week?api_key=7b4c22a6e2407b6b1d1523351e31c484";
+        //String url = "https://api.themoviedb.org/3/movies/get-popular-movies_key=7b4c22a6e2407b6b1d1523351e31c484";
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060_key=7b4c22a6e2407b6b1d1523351e31c484";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -26,8 +25,11 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
         // exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get(key:"original title"));
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("backdrop_path"));
+            System.out.println(filme.get("vote average"));
+            System.out.println();
         }
     }
 
--- src/JsonParser.java
+++ src/JsonParser.java
@@ -10,7 +10,7 @@ public class JsonParser {
     private static final Pattern REGEX_ITEMS = Pattern.compile(".\\[(.+)\\].");
     private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
     
-    List<Map<String, String>> parse(String json) {
+    public List<Map<String, String>> parse(String json) {
         Matcher matcher = REGEX_ITEMS.matcher(json);
         if (!matcher.find()) {
 
